7c4b36392344b98139b7de931eb5c53a03ee017d
soywiz <soywiz@gmail.com>
2024-08-22 11:25:32 +0200

    Update to Kotlin 2.0.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ import java.util.*
 import java.util.concurrent.*
 
 plugins {
-    kotlin("multiplatform") version "2.0.0"
+    kotlin("multiplatform") version "2.0.10"
     id("com.android.library") version "8.2.2"
     id("org.jetbrains.kotlinx.kover") version "0.8.3" apply false
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.16.2"
